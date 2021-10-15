0837a8395c9fee8e83e172ce7a9c7a8bfcedf0ad
fjerbi <firasjerbiv2@gmail.com>
2021-10-15 19:30:52 +0100

    starting state management

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hi there</Text>
@@ -9,13 +9,18 @@ const HomeScreen = ({navigation}) => {
         onPress={() => navigation.navigate("Components")}
         title="Go to components Demo"
       />
-        <Button
+      <Button
         onPress={() => navigation.navigate("List")}
         title="Go to List Demo"
       />
-        <Button
+      <Button
         onPress={() => navigation.navigate("Image")}
         title="Go to Image Screen"
+      />
+
+      <Button
+        onPress={() => navigation.navigate("Counter")}
+        title="Go to Counter Screen"
       />
     </View>
   );
